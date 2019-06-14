7c7d4d02d27ae6b817e2e56c2cdc94e5f64434a4
lastwish31 <lastwish31@gmail.com>
2019-06-14 18:20:50 +0900

    document폴더 메일보내기

---
+++ document/document/메일보내기/HomeController.java
@@ -0,0 +1,65 @@
+package com.myutil.app;
+
+import java.util.Locale;
+
+import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+    @Autowired
+	 private JavaMailSender mailSender;
+    
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		return "home";
+	}
+	
+	@RequestMapping(value = "star")
+	public String star() {
+		return "star";
+	}
+	@RequestMapping(value = "arrow")
+	public String arrow() {
+		return "arrow";
+	}
+	@RequestMapping(value = "mail")
+	public String mail() {
+		return "mail";
+	}
+	@RequestMapping(value = "mailSending.do")
+	public String mailSending(HttpServletRequest req) {
+		String setfrom = "lastwish31@gmail.com";         
+	    String tomail  = req.getParameter("tomail");     // 받는 사람 이메일
+	    String title   = req.getParameter("title");      // 제목
+	    String content = req.getParameter("content");    // 내용
+	   
+	    try {
+	      MimeMessage message = mailSender.createMimeMessage();
+	      MimeMessageHelper messageHelper 
+	                        = new MimeMessageHelper(message, true, "UTF-8");
+	 
+	      messageHelper.setFrom(setfrom);  // 보내는사람 생략하거나 하면 정상작동을 안함
+	      messageHelper.setTo(tomail);     // 받는사람 이메일
+	      messageHelper.setSubject(title); // 메일제목은 생략이 가능하다
+	      messageHelper.setText(content);  // 메일 내용
+	     
+	      mailSender.send(message);
+	    } catch(Exception e){
+	      System.out.println(e);
+	    }
+	   
+	    return "redirect:mail";
+	}
+}
